67abf48fc2404d5ae484b545822527993dd36504
lakshay1239 <Administrator@NGRental29.newgen.co.in>
2018-07-12 14:00:54 +0530

    ToDoApp

--- app/dashboard-component/dashboard-component-controller.js
+++ app/dashboard-component/dashboard-component-controller.js
@@ -1,24 +1,27 @@
 var app = angular.module('dashboard').controller('dashboardCtrl',function($scope,$state){
-    $scope.itemObj={};
-    $scope.itemList=[];
-    $scope.data ="(No Description)";
-    $scope.addItem= function(){
-      if($scope.itemObj.name!=null)
-        {$scope.itemList.push(angular.copy($scope.itemObj));}
-    }
+  $scope.itemObj={};
+  $scope.itemList=[];
+  $scope.data ="(No Description)";
+  $scope.addItem= function(){
+    if($scope.itemObj.name!=null)
+      {
+        $scope.itemList.push(angular.copy($scope.itemObj));
+        $scope.itemObj.name=null;
+      }
+  }
 
-  $scope.delete = function(index){
-    $scope.itemList.splice(index, 1); 
-    $scope.data = "(No Description)";
-    
-  };
+$scope.delete = function(index){
+  $scope.itemList.splice(index, 1); 
+  $scope.data = "(No Description)";
+  
+};
 
-  $scope.mouseClick = function (data) {
-    if($scope.itemObj.description!=null)
-    {$scope.data = data.description;}
-    else
-    {$scope.data = "(No Description)";}
- };
+$scope.mouseClick = function (data) {
+  if($scope.itemObj.description!=null)
+  {$scope.data = data.description;}
+  else
+  {$scope.data = "(No Description)";}
+};
 //  $scope.mouseLeave = function () {
 //     $scope.data = '';
 //  };
